92aa16f2d1dbedfa0cbbfaf62c730179a8379d49
olyv <a.n.lyvin@gmail.com>
2024-10-08 09:11:26 +0200

    - scaffold tests - add classes

---
+++ src/main/java/com/scoreboard/Scoreboard.java
@@ -0,0 +1,5 @@
+package com.scoreboard;
+
+public class Scoreboard {
+
+}
---
+++ src/test/java/com/scoreboard/ScoreboardTest.java
@@ -0,0 +1,27 @@
+package com.scoreboard;
+
+import org.junit.jupiter.api.Test;
+
+class ScoreboardTest {
+
+    @Test
+    public void shouldStartNewMatch() {
+
+    }
+
+    @Test
+    public void shouldUpdateMatchScore() {
+
+    }
+
+    @Test
+    public void shouldFinishMatchInProgress() {
+
+    }
+
+    @Test
+    public void shouldGetSummaryOfMatchesInProgress() {
+
+    }
+
+}
