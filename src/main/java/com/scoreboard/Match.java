39413038f8a8f229ed2d55769e2f663ea6bc9d63
olyv <a.n.lyvin@gmail.com>
2024-10-09 14:53:09 +0200

    implement ordering of summary of matches by latest update date when total scores are equal, shorten assertion

--- src/main/java/com/scoreboard/Match.java
+++ src/main/java/com/scoreboard/Match.java
@@ -1,22 +1,18 @@
 package com.scoreboard;
 
-public final class Match {
+import java.time.LocalDateTime;
+
+public class Match {
 
     private final String homeTeam;
     private final String awayTeam;
     private int homeTeamScore;
     private int awayTeamScore;
+    private LocalDateTime latestUpdate;
 
     public Match(String homeTeam, String awayTeam) {
         this.homeTeam = homeTeam;
         this.awayTeam = awayTeam;
-    }
-
-    public Match(String homeTeam, String awayTeam, int homeTeamScore, int awayTeamScore) {
-        this.homeTeam = homeTeam;
-        this.awayTeam = awayTeam;
-        this.homeTeamScore = homeTeamScore;
-        this.awayTeamScore = awayTeamScore;
     }
 
     public String getHomeTeam() {
@@ -41,5 +37,13 @@ public final class Match {
 
     public void setAwayTeamScore(int awayTeamScore) {
         this.awayTeamScore = awayTeamScore;
+    }
+
+    public LocalDateTime getLatestUpdate() {
+        return latestUpdate;
+    }
+
+    public void setLatestUpdate(LocalDateTime updated) {
+        this.latestUpdate = updated;
     }
 }
